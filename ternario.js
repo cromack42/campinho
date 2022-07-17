85279eb35a5875200090b29d56b2ae2bbcfe137e
cromack42 <74430450+cromack42@users.noreply.github.com>
2022-07-17 14:05:11 -0300

    Add files via upload

---
+++ ternario.js
@@ -0,0 +1,12 @@
+let euSouaLenda = true;
+
+let quemSouEu = euSouaLenda ? 'Você é o Will Smith' : 'Você não é a lenda!' 
+
+
+
+let minhaSenha = 100;
+let senhaAtual = 900;
+
+let suaVez = minhaSenha === senhaAtual ? 'É a sua vez' : 'Não é a sua vez!'
+
+console.log(suaVez)
